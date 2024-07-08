611dd27af8f5c5f3c6a755971134c60efbfb13c8
bader <mezzibader159@gmail.com>
2024-07-08 23:21:13 +0100

    Add initial UI components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  >
+      <Header />
+      <AddTaskForm />
+      {/* <Tasks /> */}
     </div>
   );
 }
 
 export default App;
+
+
+function Header() {
+  return <div className='flex justify-between bg-gray-500/[0.2] rounded h-16 m-5 p-3' >
+    <img className='w-10  mx-2 cursor-pointer ' src={logo} alt="logo" />
+    <div className='flex w-fit '>
+      <span className='bg-black mx-2 my-1  p-4 rounded-full cursor-pointer '></span>
+      <span className='bg-white mx-2 my-1 p-4 rounded-full  cursor-pointer'></span>
+    </div>
+  </div>
+}
+
+function AddTaskForm() {
+  return <div className='flex justify-center items-center bg-gray-500/[0.2] rounded m-5 p-3 h-16' >
+    <form className=' '>
+      <input placeholder='New task' className=' bg-white/[0.9] px-3 py-1 mx-2 rounded' />
+      <button className='bg-white/[0.9] p-1 px-4 mx-2 font-bold rounded cursor-pointer'>ADD</button>
+    </form>
+  </div>
+}
