e20cb0697788bc830de0ee1be1ded49c926649ea
bader <mezzibader159@gmail.com>
2024-07-11 18:22:31 +0100

    finalize app

--- src/App.js
+++ src/App.js
@@ -108,12 +108,9 @@ export default App;
 
 
 function Header() {
-  return <div className='flex justify-between bg-gray-500/[0.2] rounded h-16 m-5 p-3' >
+  return <div className='flex justify-center bg-gray-500/[0.2] rounded h-16 m-5 p-3' >
     <img className='w-10  mx-2 cursor-pointer drop-shadow-glow' src={logo} alt="logo" />
-    <div className='flex w-fit '>
-      <span className='bg-black mx-2 my-1  p-4 rounded-full cursor-pointer '></span>
-      <span className='bg-white mx-2 my-1 p-4 rounded-full  cursor-pointer'></span>
-    </div>
+
   </div>
 }
 
